bff8cc23c90a0daf1173ee5603080fd36cadf7e9
Oldřich Pulkrt <O.Pulkrt@gmail.com>
2014-01-04 09:20:48 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Oldřich
+ *
+ * Created on 4. leden 2014, 9:17
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
