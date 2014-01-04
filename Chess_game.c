d7139ea37b100517ce6a42e8b711cc88a2e92a6a
Oldřich Pulkrt <O.Pulkrt@gmail.com>
2014-01-04 10:44:54 +0100

    Added structure for chess game

---
+++ Chess_game.c
@@ -0,0 +1,9 @@
+
+struct Chess_game
+{
+    /* In this array are position of figure (only figures without color) */
+    int **BOARD_FIGURES;
+    
+    /* In this array are color of figure on fields */
+    int **BOARD_COLORS;
+};
