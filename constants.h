df8425d56d5119d3df88a37ec014cf528a126323
Oldřich Pulkrt <O.Pulkrt@gmail.com>
2014-01-04 10:40:38 +0100

    Added constants

---
+++ constants.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   constants.h
+ * Author: Oldřich Pulkrt <O.Pulkrt@gmail.com>
+ *
+ * Created on 4. leden 2014, 10:33
+ */
+
+#define CHESS_BOARD 8           /* Length of chess board */
+#define WHITE_CHESSPIECE 1      /* Represents white color of chess piece */
+#define BLACK_CHESSPIECE 0      /* Represents black color of chess piece */
+#define PIECE_PAWN 0            /* Represents pawn (= pesec) */
+#define PIECE_ROOK 1            /* Represents rook (= vez) */
+#define PIECE_KNIGHT 2          /* Represents knight (= jezdec) */
+#define PIECE_BISHOP 3          /* Represents bishop (= strelec) */
+#define PIECE_QUEEN 4           /* Represents queen (= dama) */
+#define PIECE_KING 5            /* Represents king (= kral) */
+
